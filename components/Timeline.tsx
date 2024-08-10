207dad273881092720ca2afe86f5c8a6dd5ed0cf
Bryan Li <bryanli200282@gmail.com>
2024-08-10 18:17:16 -0400

    fix the timeline feature but not complete yet

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -12,18 +12,39 @@ const Timeline = () => {
   return (
     <div className="relative flex justify-center ">
       <section id='experience' >
-        <Title title="Timeline" />
-        <VerticalTimeline>
+        <Title title="Experience" />
+        <VerticalTimeline >
           {experiencesData.map((item, index) => (
             <React.Fragment key={index}>
               <VerticalTimelineElement
               key={index}
               visible={true}
-              className="text-black vertical-timeline-element--work s-12"
+              className=" vertical-timeline-element--work"
+              contentStyle={{
+                background: "rgba(255, 255, 255, 0.1)",
+                borderRadius: "0.75rem", // rounded-xl
+                paddingLeft: "1.25rem", // px-5
+                paddingRight: "1.25rem", // px-5
+                paddingTop: "0.75rem", // py-3
+                paddingBottom: "0.75rem", // py-3
+                boxShadow: "none",
+                border: "1px solid rgba(0, 0, 0, 0.05)",
+                textAlign: "left",
+                padding: "1.3rem 2rem"  
+              }}
+              contentArrowStyle={{
+                borderRight:"0.4rem solid #9ca3af",
+              }}
+              date={item.date}
+              icon ={item.icon}
+              iconStyle={{
+                background: "black",
+                fontSize: "1.5rem"
+              }}
             >
-                <h3 className="vertical-timeline-element-title"> {item.title}</h3>
-                <p>{item.location}</p>
-                <p>{item.description}</p>
+                <h3 className="font-semibold capitalize"> {item.title}</h3>
+                <p className="!font-normal !mt-0">{item.location}</p>
+                <p className="!mt-1 !font-normal text-slate-400">{item.description}</p>
               </VerticalTimelineElement>
             </React.Fragment>
           ))}
--- lib/data.tsx
+++ lib/data.tsx
@@ -2,59 +2,58 @@ import { FaLocationArrow } from "react-icons/fa";
 import React from "react";
 
 export const experiencesData = [
-    {
-      title: "Software Engineer at CVS",
-      location: "Miami, FL",
-      description:
-        "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-      icon: <FaLocationArrow />,
-      date: "May - August 2024",
-    },
-    {
-      title: "Front-End Developer",
-      location: "Orlando, FL",
-      description:
-        "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-        icon: <FaLocationArrow />,
-      date: "2019 - 2021",
-    },
-    {
-      title: "Full-Stack Developer",
-      location: "Houston, TX",
-      description:
-        "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-      icon: <FaLocationArrow />,
-      date: "2021 - present",
-    },
-  ] as const;
+  {
+    title: "Full-Stack Developer",
+    location: "Houston, TX",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: <FaLocationArrow />,
+    date: "2021 - present",
+  },
 
+  {
+    title: "Teaching Assistant",
+    location: "Philadelphia, PA",
+    description:
+      "Enhancing quiz and exam performance of 20+ students by 15% through coding review sessions and provide support to students with office hours.",
+    icon: <FaLocationArrow />,
+    date: "May 2022 - August 2022",
+  },
 
+  {
+    title: "Software Engineer at CVS",
+    location: "New York, NY",
+    description:
+      "Worked in the data engineering team, Migrate Data from Hadoop to GCP.",
+    icon: <FaLocationArrow />,
+    date: "May 2024 - August 2024",
+  },
+] as const;
 
 export const skillsData = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "TypeScript",
-    "React",
-    "Next.js",
-    "Node.js",
-    "Git",
-    "Tailwind",
-    "MongoDB",
-    "Express",
-    "PostgreSQL",
-    "Python",
-    "AWS",
-    "GCP",
-    "AirFlow",
-    "Java",
-    "C/C++"
-  ] as const;
-
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "MongoDB",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "AWS",
+  "GCP",
+  "AirFlow",
+  "Java",
+  "C/C++",
+] as const;
 
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
+  { name: "Experience", link: "#experience" },
   { name: "Contact", link: "#contact" },
 ];
