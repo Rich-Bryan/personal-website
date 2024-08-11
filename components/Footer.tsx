15271dceff0b74adae30d40e59b98c0c7984ef14
Bryan Li <bryanli200282@gmail.com>
2024-08-10 20:14:56 -0400

    added the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { FaGithub } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
 const Footer = () => {
   return (
-    <footer className="mb-10 px-4 text-center text-gray-500 pt-10 flex flex-col justify-center items-center">
+    <footer id="contact" className="mb-10 px-4 text-center text-gray-500 pt-10 flex flex-col justify-center items-center">
       <small className="block text-xs mb-2">
         &copy; 2025 Bryan. All rights reserved.
       </small>
       <div className="flex gap-4">
         <a className="bg-white text-black dark:bg-white/10 dark:text-white/80 gap-2 rounded-full focus:scale-100 hover:scale-105 active:scale-100 transition flex justify-center items-center p-2"
-           href="https://github.com/Rich-Bryan" target="_blank">
+           href="mailto:bryanli082002@gmail.com" target="_blank">
           <MdEmail size="20px" />
         </a>
         <a className="bg-white text-black dark:bg-white/10 dark:text-white/80 gap-2 rounded-full focus:scale-100 hover:scale-105 active:scale-100 transition flex justify-center items-center p-2"
-           href="https://github.com/Rich-Bryan" target="_blank">
-          <FaGithub size="20px" />
+           href="tel:+12676325492" target="_blank">
+          <FaPhone  size="20px" />
         </a>
       </div>
     </footer>
